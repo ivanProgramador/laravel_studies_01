2ecd07f8eaf47bf61b4a95805b74266e6851b0b5
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-07-14 00:41:58 -0300

    controllers de ação unica

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,36 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index(){
-         echo'index';
+    //coloquei uma string do lado da função pra avisar que esse metodo retorna uma string 
+    //caso ele retornasse apenas um echo eu teria de colocar "void" não é obrigatório especificar   
+    // mas torna o codigo mais consistente quando se sabe oque esperar de um método. 
+
+    public function initMethod():string 
+    {
+        return 'Hello World';
     }
 
-    public function about(){
-         echo'about';
+    //Nesse caso esse metodo vai retornar um componente da classe view
+    //então o retorno tem que ser do tipo view 
+
+    public function viewPage():View 
+    {
+        return view('home');
     }
 
-    public function mostrarValor($valor){
-         echo'valor vindo da rota '.$valor;
-    }
 
-    public function mostrarValores($valor1,$valor2){
-         echo'valores vindos da rota '.$valor1.' e '.$valor2;
-    }
-
-    //se o valor não for enviado ele vai ficar como nulo 
-    public function mostrarValorOpcional($valor = null){
-         echo 'valor opcional vindo da rota '.$valor;
-    }
-
-    //se o valor não for enviado ele vai ficar como nulo 
-    //é importante que um valor default seja definido mesmo que seja um vaor nulo pra evitar 
-    //que a aplicação mostre erros ou travamentos para cliente visando evitar que o sistema perca credibilidade.
-
-    public function mostrarValorOpcional2($valor1,$valor2 = null){
-         echo 'Esse varor e obrigatorio : '.$valor1.' esse aqui não é : '.$valor2;
-    }
 }
